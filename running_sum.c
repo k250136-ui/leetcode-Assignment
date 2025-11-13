09854aeee7d8b325d96a6a33407dddd5bcc2a9be
k250136-ui <k250136@nu.edu.pk>
2025-11-13 20:13:18 +0500

    1480

---
+++ running_sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    
+    int nums[n];
+    for(int i=0;i<n;i++){
+        scanf("%d", &nums[i]);
+    }
+
+    int runningSum[n];
+    runningSum[0] = nums[0];
+
+    for(int i=1;i<n;i++){
+        runningSum[i] = runningSum[i-1] + nums[i];
+        printf("%d, ", runningSum[i]);
+    }
+    return 0;
+}
